f89148d1354be4837c05d007d6bf02cfcbe09906
Justin <116049066+Tushigeee@users.noreply.github.com>
2024-01-03 18:33:49 -0800

    added user id in backend

--- backend/controllers/products/createProduct.js
+++ backend/controllers/products/createProduct.js
@@ -1,26 +1,31 @@
 const Product = require("../../models/product");
+
 const createProduct = async (req, res) => {
   const { name, price, description, category } = req.body;
+  const userId = req.user._id;
+
+  if (!name || !price || !description || !category || !userId) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+
   try {
-    if (!name || !price || !description || !category) {
-      return res.status(400).json({
-        message: "All fields are required",
-      });
-    } else {
-      const product = await Product.create({
-        name,
-        price,
-        description,
-        category,
-      });
-      res.status(201).json(product);
-    }
+    const product = await Product.create({
+      name,
+      price,
+      description,
+      category,
+      userId,
+    });
+    res.status(201).json(product);
   } catch (err) {
     return res.status(500).json({
       message: err.message,
     });
   }
 };
+
 module.exports = {
   createProduct,
 };
